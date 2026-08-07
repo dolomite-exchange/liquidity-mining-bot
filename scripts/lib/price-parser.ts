5cdfb64b9235923f1a64715745523a0fc70443f4
Corey <corey@dolomite.io>
2026-08-07 17:31:51 -0400

    fixed comp error;

--- scripts/lib/price-parser.ts
+++ scripts/lib/price-parser.ts
@@ -1,4 +1,5 @@
 import { BigNumber, Decimal, Integer } from '@dolomite-exchange/dolomite-margin';
+import { sleep } from '../../src/helpers/async-helpers';
 import { dolomite } from '../../src/helpers/web3';
 import { getWeb3RequestWithBackoff } from './web3-helper';
 
